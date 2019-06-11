0398a83d5bc2389e4b43fee4f97427bb9ee71f98
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2019-06-11 13:14:36 +0100

    Added sock emitting to send and receive messages which are then stored within Vue data messages on the client

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ var io = require('socket.io')(server);
 server.listen(3000);
 
 app.get('/', function (request, response) {
-    response.send(__dirname + '/index.html');
+    response.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
-    console.log('A connection was made');
+    socket.on('chat.message', function(message) {
+        console.log('New Message', message);
+        socket.emit('chat.message', message);
+    });
 });
